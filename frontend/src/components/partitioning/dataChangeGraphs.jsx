39a67de0ca6296bb2143133771c1e514296c846c
oussamahadjaissafekhar <jo_hadjaissafekhar@esi.dz>
2024-08-23 17:18:44 +0100

    Fixed the titles of analyze workload charts , The partitioning charts added are : BarChart , StackedBarhart , PieChart for all the attributes

--- frontend/src/components/partitioning/dataChangeGraphs.jsx
+++ frontend/src/components/partitioning/dataChangeGraphs.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import BarChart from './workloadAnalyzerCharts/BarChart';
-import StackedBarChart from './workloadAnalyzerCharts/StackedBarChart';
-import '../css/Modal.css';
+import BarChart from '../partitioningCharts/BarChart';
+import StackedBarChart from '../partitioningCharts/StackedBarChart';
+import PieChart from '../partitioningCharts/PieChart';
+import '../../css/Modal.css';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement } from 'chart.js';
 
 // Register all necessary components
@@ -15,12 +16,13 @@ ChartJS.register(
     ArcElement
 );
 
-function Modal({ data, onClose }) {
+function DataChangeGraphs({ data, onClose }) {
     const [currentChart, setCurrentChart] = useState(0);
 
     const charts = [
         <BarChart data={data} key="bar" />,
-        <StackedBarChart data={data} key="stacked-bar" />
+        <StackedBarChart data={data} key="stacked-bar" />,
+        <PieChart data={data} key="pie-chart"/>
     ];
 
     if (!data || data.length === 0) {
@@ -47,27 +49,25 @@ function Modal({ data, onClose }) {
             <div className="modal-content">
                 <button className="close-button" onClick={onClose}>X</button>
                 <div className="chart-container">
-                <img 
-                    src={require('../icons/left-arrow.png')} 
-                    alt="Previous" 
-                    className="nav-button prev" 
-                    onClick={handlePrevious}
-                />
+                    <img 
+                        src={require('../../icons/left-arrow.png')} 
+                        alt="Previous" 
+                        className="nav-button prev" 
+                        onClick={handlePrevious}
+                    />
                     <div className="chart">
                         {charts[currentChart]}
                     </div>
                     <img 
-                    src={require('../icons/right-arrow.png')} 
-                    alt="Next" 
-                    className="nav-button next" 
-                    onClick={handleNext}
-                />
+                        src={require('../../icons/right-arrow.png')} 
+                        alt="Next" 
+                        className="nav-button next" 
+                        onClick={handleNext}
+                    />
                 </div>
-                
-                
             </div>
         </div>
     );
 }
 
-export default Modal;
+export default DataChangeGraphs;
